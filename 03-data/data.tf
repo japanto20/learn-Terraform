ece9c15569aecf1ceba317d1d703471e5c9bcf1b
Anto <japanto20@gmail.com>
2023-09-27 19:01:26 -0500

    Create servers

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -1,9 +1,9 @@
-data "aws_ami" "ami" {
+data "aws_ami" "anto" {
   most_recent = true
   name_regex  = "Centos-8-DevOps-Practice"
   owners      = ["484704032921"]
 }
 
 output "ami_id" {
-  value = data.aws_ami.ami.id
+  value = data.aws_ami.anto.image_id
 }
