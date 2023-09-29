0250f225b0e54ebd4ba6ef8f81f6049c0f76700c
Anto <japanto20@gmail.com>
2023-09-28 19:02:16 -0500

    Create servers

---
+++ roboshop-setup/ex1/servers.tf
@@ -0,0 +1,107 @@
+resource "aws_instance" "frontend" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "frontend"
+  }
+}
+
+
+resource "aws_instance" "cart" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "cart"
+  }
+}
+
+
+
+resource "aws_instance" "catalogue" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.small"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "catalogue"
+  }
+}
+
+
+
+resource "aws_instance" "user" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "user"
+  }
+}
+
+
+
+resource "aws_instance" "shipping" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "shipping"
+  }
+}
+
+
+
+resource "aws_instance" "payment" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "payment"
+  }
+}
+
+
+
+resource "aws_instance" "mongodb" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "mongodb"
+  }
+}
+
+
+
+resource "aws_instance" "mysql" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "mysql"
+  }
+}
+
+
+
+resource "aws_instance" "redis" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "redis"
+  }
+}
+
+
+resource "aws_instance" "rabbitmq" {
+  ami                    = "ami-03265a0778a880afb"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
+  tags = {
+    Name = "rabbitmq"
+  }
+}
+
+
