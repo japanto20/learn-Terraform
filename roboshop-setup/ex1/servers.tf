7bce815f3a47d53f4ef81c3204944f57404f1104
Anto <japanto20@gmail.com>
2023-10-05 12:00:22 -0500

    Create servers

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ami" {
 
 
 resource "aws_instance" "frontend" {
-  ami                    = "ami-03265a0778a880afb"
+  ami                    = data.aws_ami.ami.image_id
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
   tags = {
