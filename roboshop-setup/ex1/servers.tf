4e321016bc827168827744149404da658dc1c7d0
Anto <japanto20@gmail.com>
2023-10-05 12:02:03 -0500

    Create servers

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -1,12 +1,5 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["484704032921"]
-}
-
-
 resource "aws_instance" "frontend" {
-  ami                    = data.aws_ami.ami.image_id
+  ami                    = "ami-03265a0778a880afb"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
   tags = {
--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -1,13 +1,7 @@
-data "aws_ami" "ami" {
-  most_recent = true
-  name_regex  = "Centos-8-DevOps-Practice"
-  owners      = ["484704032921"]
-}
-
 
 resource "aws_instance" "frontend" {
   count = 5
-  ami                    = data.aws_ami.ami.image_id
+  ami                    = "ami-03265a0778a880afb"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
 
