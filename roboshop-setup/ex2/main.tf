23e074a396484fe8ad6627283c04db92e96e69cc
Anto <japanto20@gmail.com>
2023-10-05 12:06:01 -0500

    Create servers

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -1,8 +1,12 @@
 
 resource "aws_instance" "frontend" {
-  count = 5
+  count                  = length(var.instances)
   ami                    = "ami-03265a0778a880afb"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0e02eb30681e3cecb"]
 
   }
+
+variable "instances"{
+  default = ["cart", "catalogue", "user", "payment", "shipping"]
+}
