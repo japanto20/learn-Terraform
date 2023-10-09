e2c18ba58813ece64756a3677f3d640df1dd865d
Anto <japanto20@gmail.com>
2023-10-09 18:25:11 -0500

    Create servers

---
+++ roboshop-setup/ex4/ec2/main.tf
@@ -0,0 +1,20 @@
+resource "aws_instance" "ec2" {
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = var.instance_type
+  vpc_security_group_ids = [var.sg_id]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z07168053N2UH00BX82ND"
+  name    = "${var.component}-dev.devopsb71.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
+variable "sg_id" {}
